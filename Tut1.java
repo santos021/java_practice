edea9149e6ae5cf6684a751ed7fbae23a02068d7
santos021 <santosbiswal543@gmail.com>
2022-12-14 20:36:36 +0530

    ..

---
+++ Tut1.java
@@ -0,0 +1,44 @@
+// to print:
+// *
+// **
+// ***
+// ****
+
+class Tut1 {
+    public static void main (String[] args)
+    {
+        // output in java
+
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+    
+}
+
+
+// Variable in java
+
+class Tut2 {
+    public static void main (String[] args)
+    {
+        // Variables 
+
+        int x = 10;
+        int y = 35;
+        int sum = x + y;
+        System.out.println(sum);
+
+        int mul = x * y;
+        System.out.println(mul);
+
+        // solve a problem: (a*b) / (b-a) if a = 10 , b = 20 ?
+
+        int a = 10;
+        int b = 20;
+        int ans = (a*b) / (b-a);
+        System.out.println(ans);
+
+    }
+}
