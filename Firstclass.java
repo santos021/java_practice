d3ae2a6d46d6b3ae835298708a70b2fc70f95d80
santos021 <santosbiswal543@gmail.com>
2022-12-14 20:34:20 +0530

    ..

---
+++ Firstclass.java
@@ -0,0 +1,8 @@
+class Firstclass {
+    public static void main(String[] args) {
+        System.out.print("Hello Santos!!\n");
+        System.out.println("Hello Santos!!");
+        System.out.print("Hello Santos!!");
+    }
+
+}
